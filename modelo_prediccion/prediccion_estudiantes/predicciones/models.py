514502a253afd4d2a610ebb431bd201d0ff89075
IVAINX18 <ivainxsanchez18@gmail.com.com>
2025-11-17 11:08:43 -0500

    MOdelo entrenado

--- modelo_prediccion/prediccion_estudiantes/predicciones/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Prediccion(models.Model):
-    nombre = models.CharField(max_length=100)
-    resultado = models.FloatField()
-    fecha = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.nombre} - {self.resultado:.2f}"
